228090d52143748f008d598a4dd64adfa268ffe1
alexey <alexey@digger.dev>
2023-07-24 16:53:17 +0100

    add frontegg config

--- controllers/web.go
+++ controllers/web.go
@@ -3,11 +3,14 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"time"
+	"os"
 )
 
 func MainPage(c *gin.Context) {
+	url := os.Getenv("FRONTEGG_URL")
+	clientId := os.Getenv("FRONTEGG_CLIENT_ID")
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"now": time.Date(2017, 0o7, 0o1, 0, 0, 0, 0, time.UTC),
+		"FronteggUrl":      url,
+		"FronteggClientId": clientId,
 	})
 }
