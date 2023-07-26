dbd8cf6f26a8feeac3431edc94534e79aa9835ea
alexey <alexey@digger.dev>
2023-07-26 15:21:43 +0100

    remove unused code

--- controllers/web.go
+++ controllers/web.go
@@ -4,11 +4,8 @@ import (
 	"digger.dev/cloud/config"
 	"digger.dev/cloud/middleware"
 	"digger.dev/cloud/models"
-	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
-	"log"
 	"net/http"
 )
 
@@ -26,39 +23,21 @@ func (web *WebController) MainPage(c *gin.Context) {
 }
 
 func (web *WebController) ProjectsPage(c *gin.Context) {
-	requestedOrganisation := c.Param("organisation")
-	loggedInOrganisation, exists := c.Get(middleware.ORGANISATION_ID_KEY)
+	loggedInOrganisationId, exists := c.Get(middleware.ORGANISATION_ID_KEY)
 
-	fmt.Printf("read org id %v\n", loggedInOrganisation)
+	fmt.Printf("read org id %v\n", loggedInOrganisationId)
 
 	if !exists {
 		c.String(http.StatusForbidden, "Not allowed to access this resource")
 		return
 	}
 
-	var org models.Organisation
-	err := models.DB.Where("name = ?", requestedOrganisation).First(&org).Error
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			c.String(http.StatusNotFound, "Could not find organisation: "+requestedOrganisation)
-		} else {
-			c.String(http.StatusInternalServerError, "Unknown error occurred while fetching database")
-		}
-		return
-	}
-
-	if org.ID != loggedInOrganisation {
-		log.Printf("Organisation ID %v does not match logged in organisation ID %v", org.ID, loggedInOrganisation)
-		c.String(http.StatusForbidden, "Not allowed to access this resource")
-		return
-	}
-
 	var projects []models.Project
 
-	err = models.DB.Preload("Organisation").Preload("Namespace").
+	err := models.DB.Preload("Organisation").Preload("Namespace").
 		Joins("LEFT JOIN namespaces ON projects.namespace_id = namespaces.id").
 		Joins("LEFT JOIN organisations ON projects.organisation_id = organisations.id").
-		Where("projects.organisation_id = ?", org.ID).Find(&projects).Error
+		Where("projects.organisation_id = ?", loggedInOrganisationId).Find(&projects).Error
 
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Unknown error occurred while fetching database")
