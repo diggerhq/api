df384de2756ac4df020ec0698ba9ad4e672259b2
Dias Saparov <dias@digger.dev>
2023-09-26 11:59:17 +0100

    add indices and use joins

--- models/storage.go
+++ models/storage.go
@@ -556,31 +556,13 @@ func (db *Database) UpdateDiggerJob(job *DiggerJob) error {
 func (db *Database) GetPendingParentDiggerJobs() ([]DiggerJob, error) {
 	jobs := make([]DiggerJob, 0)
 
-	result := db.GormDB.Where("status = ?", DiggerJobCreated).Find(&jobs)
+	result := db.GormDB.Joins("LEFT JOIN digger_job_parent_links ON digger_jobs.digger_job_id = digger_job_parent_links.digger_job_id").Where("digger_jobs.status = ? AND digger_job_parent_links.id IS NULL", DiggerJobCreated).Find(&jobs)
 	if result.Error != nil {
 		if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
 			return nil, result.Error
 		}
 	}
-
-	filteredJobsWithNoParents := make([]DiggerJob, 0)
-
-	for _, job := range jobs {
-		parentLinks := make([]DiggerJobParentLink, 0)
-
-		result := db.GormDB.Where("digger_job_id = ?", job.DiggerJobId).Find(&parentLinks)
-
-		if result.Error != nil {
-			if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
-				return nil, result.Error
-			}
-		}
-		if len(parentLinks) == 0 {
-			filteredJobsWithNoParents = append(filteredJobsWithNoParents, job)
-		}
-	}
-
-	return filteredJobsWithNoParents, nil
+	return jobs, nil
 }
 
 func (db *Database) GetDiggerJob(jobId string) (*DiggerJob, error) {
